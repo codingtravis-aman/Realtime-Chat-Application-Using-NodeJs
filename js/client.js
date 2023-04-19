011d86ea3304d09000c424173b3a2a75d94cebc8
Satish <satishjha844@gmail.com>
2023-04-19 13:04:08 +0530

    initialcommit

---
+++ js/client.js
@@ -0,0 +1,11 @@
+const socket = io('http://localhost:8000')
+
+const from  = document.getElementById('send-container');
+
+const messageInput = document.getElementById('messageInp')
+
+const messageContainer = document.querySelector(".container")
+
+const name = prompt("Enter your to join");
+
+socket.emit('new-user-joined',name)
